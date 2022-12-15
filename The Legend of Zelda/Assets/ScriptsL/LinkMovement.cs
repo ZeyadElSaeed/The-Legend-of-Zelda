3c75d31b008142c14bc457da2c177c957fabce33
M-Konar <konar.mohamed@gmail.com>
2022-12-15 15:56:07 +0200

    merged the two branches, resolved some errors

--- The Legend of Zelda/Assets/ScriptsL/LinkMovement.cs
+++ The Legend of Zelda/Assets/ScriptsL/LinkMovement.cs
@@ -15,11 +15,11 @@ public class LinkMovement : MonoBehaviour
     public float jumpMultiplier = 500; // multplies the up jump force
     public bool isFalling;
     // Start is called before the first frame update
-    Rigidbody m_Rigidbody;
-    float m_Speed;
-    float r_Speed;
+    //Rigidbody m_Rigidbody;
+    //float m_Speed;
+    //float r_Speed;
 
-    
+
     void MoveAllDirections()
     {
         if (Input.GetKey("up"))
@@ -53,45 +53,21 @@ public class LinkMovement : MonoBehaviour
         if (Input.GetKey("a"))
         {
             transform.position += Vector3.left * Time.deltaTime * currentMoveSpeed;
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
-            transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
-            transform.Translate(Vector3.forward * m_Speed * Time.deltaTime * -1);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            //Rotate the sprite about the Y axis in the positive direction
-            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * r_Speed, Space.World);
-            // transform.translate(new Vector3(0, 1, 0) * Time.deltaTime * m_Speed, Space.World);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            //Rotate the sprite about the Y axis in the negative direction
-            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * r_Speed, Space.World);
         }
     }
-
     void Jump()
     {
-        
-            if (isFalling == false)
-            {
 
-                if (Input.GetKeyDown("space"))
-                {
+        if (isFalling == false)
+        {
+
+            if (Input.GetKeyDown("space"))
+            {
                 rb.AddForce(Vector3.up * jumpMultiplier);
                 //transform.position += Vector3.up * Time.deltaTime * jumpMultiplier;
-                }
             }
-        
+        }
+
     }
     void Sprint()
     {
@@ -123,10 +99,10 @@ public class LinkMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>(); //calling rigidbody for the gliding
         currentMoveSpeed = defaultMoveSpeed;
-        m_Rigidbody = GetComponent<Rigidbody>();
+        //m_Rigidbody = GetComponent<Rigidbody>();
         //Set the speed of the GameObject
-        m_Speed = 3.0f;
-        r_Speed = 90.0f;
+        //m_Speed = 3.0f;
+        //r_Speed = 90.0f;
     }
 
     // Update is called once per frame
@@ -136,7 +112,7 @@ public class LinkMovement : MonoBehaviour
         Sprint();
         Glide();
         Jump();
-        if (transform.position.y < y_pos && transform.position.y > plane.transform.position.y+2)
+        if (transform.position.y < y_pos && transform.position.y > plane.transform.position.y + 2)
         {
             isFalling = true;
         }
@@ -147,11 +123,12 @@ public class LinkMovement : MonoBehaviour
     //Used to controll jumping when gliding
     void OnCollisionEnter(Collision c)
     {
-        Debug.Log(LayerMask.LayerToName(c.gameObject.layer));
+        //Debug.Log(LayerMask.LayerToName(c.gameObject.layer));
         if (LayerMask.LayerToName(c.gameObject.layer).Equals("Climbable"))
         {
             y_pos = c.gameObject.transform.position.y;
-            
+
         }
     }
 }
+
