0b0d29d120cae55d5fbddf337f20b9abce98f7aa
MahmoudAbdelkhalek5o5o <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2022-12-15 08:26:55 +0200

    climb 8:26 Am

--- The Legend of Zelda/Assets/ScriptsL/LinkMovement.cs
+++ The Legend of Zelda/Assets/ScriptsL/LinkMovement.cs
@@ -5,10 +5,37 @@ using UnityEngine;
 public class LinkMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody m_Rigidbody;
+    float m_Speed;
+    float r_Speed;
 
+    
     void MoveAllDirections()
     {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
+        }
 
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+            transform.Translate(Vector3.forward * m_Speed * Time.deltaTime * -1);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Rotate the sprite about the Y axis in the positive direction
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * r_Speed, Space.World);
+            // transform.translate(new Vector3(0, 1, 0) * Time.deltaTime * m_Speed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Rotate the sprite about the Y axis in the negative direction
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * r_Speed, Space.World);
+        }
     }
 
     void Jump()
@@ -31,12 +58,15 @@ public class LinkMovement : MonoBehaviour
     }
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 3.0f;
+        r_Speed = 90.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveAllDirections();
     }
 }
