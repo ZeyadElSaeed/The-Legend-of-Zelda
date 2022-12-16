cb533fe086a0326e9975cd8f2313ae6e0dca5fe1
Zeyad ElSaeed <ziadelnagar380@gmail.com>
2022-12-16 21:24:28 +0200

    first-test

---
+++ The Legend of Zelda/Assets/Scripts/StandingState.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+
+public class StandingState: State
+{  
+    float gravityValue;
+    bool jump;   
+    bool crouch;
+    Vector3 currentVelocity;
+    bool grounded;
+    bool sprint;
+    float playerSpeed;
+    bool drawWeapon;
+
+    Vector3 cVelocity;
+
+    public StandingState(Character _character, StateMachine _stateMachine) : base(_character, _stateMachine)
+	{
+		character = _character;
+		stateMachine = _stateMachine;
+	}
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        jump = false;
+        crouch = false;
+        sprint = false;
+        drawWeapon = false;
+        input = Vector2.zero;
+        
+        currentVelocity = Vector3.zero;
+        gravityVelocity.y = 0;
+
+        velocity = character.playerVelocity;
+        playerSpeed = character.playerSpeed;
+        grounded = character.controller.isGrounded;
+        gravityValue = character.gravityValue;    
+    }
+
+    public override void HandleInput()
+    {
+        base.HandleInput();
+
+        if (jumpAction.triggered)
+        {
+            jump = true;
+		}
+		if (crouchAction.triggered)
+		{
+            crouch = true;
+		}
+		if (sprintAction.triggered)
+		{
+            sprint = true;
+		}
+
+		if (drawWeaponAction.triggered)
+		{
+            drawWeapon = true;
+        }
+
+        input = moveAction.ReadValue<Vector2>();
+        velocity = new Vector3(input.x, 0, input.y);
+
+        velocity = velocity.x * character.cameraTransform.right.normalized + velocity.z * character.cameraTransform.forward.normalized;
+        velocity.y = 0f;
+     
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        character.animator.SetFloat("speed", input.magnitude, character.speedDampTime, Time.deltaTime);
+
+        if (sprint)
+		{
+            stateMachine.ChangeState(character.sprinting);
+        }    
+        if (jump)
+        {
+            stateMachine.ChangeState(character.jumping);
+        }
+		if (crouch)
+		{
+            stateMachine.ChangeState(character.crouching);
+        }
+        if (drawWeapon)
+        {
+            stateMachine.ChangeState(character.combatting);
+            character.animator.SetTrigger("drawWeapon");
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+
+        gravityVelocity.y += gravityValue * Time.deltaTime;
+        grounded = character.controller.isGrounded;
+
+        if (grounded && gravityVelocity.y < 0)
+        {
+            gravityVelocity.y = 0f;
+        }
+       
+        currentVelocity = Vector3.SmoothDamp(currentVelocity, velocity,ref cVelocity, character.velocityDampTime);
+        character.controller.Move(currentVelocity * Time.deltaTime * playerSpeed + gravityVelocity * Time.deltaTime);
+  
+		if (velocity.sqrMagnitude>0)
+		{
+            character.transform.rotation = Quaternion.Slerp(character.transform.rotation, Quaternion.LookRotation(velocity),character.rotationDampTime);
+        }
+        
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        gravityVelocity.y = 0f;
+        character.playerVelocity = new Vector3(input.x, 0, input.y);
+
+        if (velocity.sqrMagnitude > 0)
+        {
+            character.transform.rotation = Quaternion.LookRotation(velocity);
+        }
+    }
+
+}
---
+++ The Legend of Zelda/Assets/Scripts/State.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class State
+{
+    public Character character;
+    public StateMachine stateMachine;
+
+    protected Vector3 gravityVelocity;
+    protected Vector3 velocity;
+    protected Vector2 input;
+
+    public InputAction moveAction;
+    public InputAction lookAction;
+    public InputAction jumpAction;
+    public InputAction crouchAction;
+    public InputAction sprintAction;
+    public InputAction drawWeaponAction;
+    public InputAction attackAction;
+
+    public State(Character _character, StateMachine _stateMachine)
+	{
+        character = _character;
+        stateMachine = _stateMachine;
+
+        moveAction = character.playerInput.actions["Move"];
+        lookAction = character.playerInput.actions["Look"];
+        jumpAction = character.playerInput.actions["Jump"];
+        crouchAction = character.playerInput.actions["Crouch"];
+        sprintAction = character.playerInput.actions["Sprint"];
+        drawWeaponAction = character.playerInput.actions["DrawWeapon"];
+        attackAction = character.playerInput.actions["Attack"];
+
+    }
+
+    public virtual void Enter()
+    {
+        //StateUI.instance.SetStateText(this.ToString());
+		Debug.Log("Enter State: "+this.ToString());
+    }
+
+    public virtual void HandleInput()
+    {
+    }
+
+    public virtual void LogicUpdate()
+    {
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+}
+
