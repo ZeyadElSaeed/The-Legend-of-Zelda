eed28dd58e5dfdb080f17aa62b6432cd0ea9f5ca
M-Konar <konar.mohamed@gmail.com>
2022-12-21 23:25:00 +0200

    arrows damage enemies and boss

--- The Legend of Zelda/Assets/Scripts/ArrowScript.cs
+++ The Legend of Zelda/Assets/Scripts/ArrowScript.cs
@@ -9,9 +9,30 @@ public class ArrowScript : MonoBehaviour
     public GameObject hitParticle;
 
     public ParticleSystem trailParticle;
+    public float radius = 2f;
 
     private void OnCollisionEnter(Collision collision)
     {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider arrowed in colliders)
+        {
+            if (collision.gameObject.CompareTag("Bokoblin"))
+            {
+                collision.gameObject.GetComponent<Enemy>().TakeDamage(5);
+                break;
+
+            }
+            else if (collision.gameObject.CompareTag("Moblin"))
+            {
+                collision.gameObject.GetComponent<Enemy>().TakeDamage(5);
+                break;
+            }
+            else if (collision.gameObject.CompareTag("MawJLaygo"))
+            {
+                collision.gameObject.GetComponent<Fire>().TakeDamage(5);
+                break;
+            }
+        }
         Instantiate(hitParticle, transform.position, Quaternion.identity);
         trailParticle.transform.parent = transform.parent;
         trailParticle.Stop();
