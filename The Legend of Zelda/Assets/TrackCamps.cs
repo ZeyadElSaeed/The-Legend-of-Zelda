81c2448f589c40c2ef1270547f698bdca970abb4
M-Konar <konar.mohamed@gmail.com>
2022-12-25 20:02:29 +0200

    .

--- The Legend of Zelda/Assets/TrackCamps.cs
+++ The Legend of Zelda/Assets/TrackCamps.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenShrine : MonoBehaviour
+public class TrackCamps : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    // Update is called once per frame
+    void Update()
     {
-        Debug.Log("Shrine opened");
+        
     }
-
 }
