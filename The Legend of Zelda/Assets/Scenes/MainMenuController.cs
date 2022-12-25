21cb5ab4244bd6828ab3e27d3899e3944f167b2f
M-Konar <konar.mohamed@gmail.com>
2022-12-25 23:32:56 +0200

    Main menu integrated with the game
    Co-Authored-By: Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>

---
+++ The Legend of Zelda/Assets/Scenes/MainMenuController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Main Menu UI")]
+    [SerializeField] GameObject MainButtons;
+    [SerializeField] GameObject LevelButtons;
+
+    private void Start()
+    {
+        LevelButtons.SetActive(false);
+        MainButtons.SetActive(true);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Overworld");
+    }
+
+    public void SelectLevel()
+    {
+        Debug.Log("Select Level");
+        MainButtons.SetActive(false);
+        LevelButtons.SetActive(true);
+    }
+
+    public void ReturnToMainButtons()
+    {
+        LevelButtons.SetActive(false);
+        MainButtons.SetActive(true);
+    }
+    public void SelectOverWorld()
+    {
+        Debug.Log("Select OverWorld");
+        SceneManager.LoadScene("Overworld");
+    }
+    public void SelectShrine()
+    {
+        Debug.Log("Select Shrine");
+    }
+    public void SelectFireBlightArena()
+    {
+        Debug.Log("Select Fire");
+    }
+    public void SelectHinixArena()
+    {
+        Debug.Log("Select Hinix");
+    }
+
+    public void GoToSettingMenu()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+ 
+}
