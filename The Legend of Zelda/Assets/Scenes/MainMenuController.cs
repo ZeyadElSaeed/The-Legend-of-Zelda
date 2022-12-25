858a460d77b898a4751c4bc955369fb2563487dd
Zeyad ElSaeed <ziadelnagar380@gmail.com>
2022-12-25 19:58:49 +0200

    setting, team credits

--- The Legend of Zelda/Assets/Scenes/MainMenuController.cs
+++ The Legend of Zelda/Assets/Scenes/MainMenuController.cs
@@ -5,10 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void PlayGame()
+    [Header("Main Menu UI")]
+    [SerializeField] GameObject MainButtons;
+    [SerializeField] GameObject LevelButtons;
+
+    private void Start()
+    {
+        LevelButtons.SetActive(false);
+        MainButtons.SetActive(true);
+    }
+
+    public void NewGame()
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void SelectLevel()
+    {
+        Debug.Log("Select Level");
+        MainButtons.SetActive(false);
+        LevelButtons.SetActive(true);
+    }
+
+    public void ReturnToMainButtons()
+    {
+        LevelButtons.SetActive(false);
+        MainButtons.SetActive(true);
+    }
+    public void SelectOverWorld()
+    {
+        Debug.Log("Select OverWorld");
+    }
+    public void SelectShrine()
+    {
+        Debug.Log("Select Shrine");
+    }
+    public void SelectFireBlightArena()
+    {
+        Debug.Log("Select Fire");
+    }
+    public void SelectHinixArena()
+    {
+        Debug.Log("Select Hinix");
+    }
+
     public void GoToSettingMenu()
     {
         SceneManager.LoadScene("SettingMenu");
---
+++ The Legend of Zelda/Assets/Scenes/SettingController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingController : MonoBehaviour
+{
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void GoToTeamCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void GoToAssetsCredits()
+    {
+        Debug.Log("to assets credits");
+        //SceneManager.LoadScene("Credits");
+    }
+
+}
