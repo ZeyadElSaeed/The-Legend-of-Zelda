594c4e9cd6258c5f2f7fc7ec5316f711b7143bdd
M-Konar <konar.mohamed@gmail.com>
2022-12-26 17:05:47 +0200

    .

--- The Legend of Zelda/Assets/Scripts/OpenShrine.cs
+++ The Legend of Zelda/Assets/Scripts/OpenShrine.cs
@@ -7,10 +7,7 @@ public class OpenShrine : MonoBehaviour
     [SerializeField] GameObject[] enemies;
 
 
-    private void Start()
-    {
-        enemies = new GameObject[12];
-    }
+    
     private void OnTriggerEnter(Collider other)
     {
         for(int i =0; i< enemies.Length; i++)
