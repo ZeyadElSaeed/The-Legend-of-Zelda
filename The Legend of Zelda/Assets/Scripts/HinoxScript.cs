e1033e52452af49b434ef41d6a2ac65c26b2a275
purplereignn <ahgebril@windowslive.com>
2022-12-28 20:23:52 +0200

    HinoxArena IMproved

--- The Legend of Zelda/Assets/Scripts/HinoxScript.cs
+++ The Legend of Zelda/Assets/Scripts/HinoxScript.cs
@@ -11,7 +11,6 @@ public class HinoxScript : MonoBehaviour
 {
     public float health = 150;
     private Animator anim;
-    //public bool Phase2 = false;
     private int treeIndex = 0;
     public Slider HealthBar;
     public TextMeshProUGUI HealthValue;
@@ -24,8 +23,7 @@ public class HinoxScript : MonoBehaviour
     private GameObject player;
     [SerializeField] float offsetY;
     public bool isHit;
-    private bool isDead;
-    // Start is called before the first frame update
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -33,11 +31,8 @@ public class HinoxScript : MonoBehaviour
         anim = GetComponent<Animator>();
         anim.SetBool("Phase1idle",true);
         TreeInHand.SetActive(false);
-        //Phase2 = false;
         treeIndex = 0;
-        //TakeDamage(60);
         isHit = false;
-        isDead = false;
     }
 
     // Update is called once per frame
@@ -47,10 +42,6 @@ public class HinoxScript : MonoBehaviour
         HealthValue.text = "Hinox "+health;
         if(health<0)
             HealthValue.text = "Hinox 0";
-
-
-
-        //TakeDamage(150);
     }
 
     public void Shoot(){
@@ -69,11 +60,10 @@ public class HinoxScript : MonoBehaviour
     }
 
     public void GotoNextTree(){
-        //Debug.Log(treeIndex);
+
         if(anim.GetBool("Phase2Attack")==false)
             return;
         
-        //Debug.Log(treeIndex);
 
         if(treeIndex >= 7){
             anim.SetBool("Phase2Attack",false);
@@ -82,14 +72,6 @@ public class HinoxScript : MonoBehaviour
 
         TreeGrabbed = Trees[treeIndex];
         treeIndex++;
-        // if((!agent.pathPending && agent.remainingDistance <= 0) || treeIndex == 0){
-        //     agent.SetDestination(Trees[treeIndex].transform.position);
-        //     TreeGrabbed = Trees[treeIndex];
-        //     treeIndex = (treeIndex + 1);
-        // }else{
-        //     anim.SetTrigger("Grabbed");
-        //     TreeInHand.SetActive(true);
-        //     TreeGrabbed.SetActive(false);
     }
 
     public void TakeDamage(float damage){
@@ -98,24 +80,18 @@ public class HinoxScript : MonoBehaviour
         isHit = true;
         if (health <= 0){
             anim.SetTrigger("Die");
-            isDead = true;
             GetComponent<Collider>().enabled = false;
             StartCoroutine(dieWaitTime());
         }if(health <= 100 && treeIndex < 7){
-            //Phase2 = true;
             anim.SetBool("Phase1idle",false);
-            anim.SetBool("Phase2Attack",true);
-        }if(!anim.GetBool("Phase2Attack")&&!isDead)
-            anim.SetTrigger("Hit");
+            anim.SetBool("Phase2Attack",true);}
     }
+    
     IEnumerator dieWaitTime()
     {
-
-
         yield return new WaitForSeconds(5);
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("Credits");
-
     }
 
     public void StartDealDamage()
